9d68bb477b9a67d960f02bf9c323b5044d950fdf
Danyil Tkachuk <d.tkachyk@e-u.edu.ua>
2026-03-29 22:45:39 +0300

    feat: configure alembic and apply first migration

--- alembic/env.py
+++ alembic/env.py
@@ -19,6 +19,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.database import Base
+from app import models
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
